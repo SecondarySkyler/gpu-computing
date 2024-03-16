ba6d49236a1d63f25c22d4cb1fcaf0c5a647ecb6
Cristian <skylerhunter6@gmail.com>
2024-03-16 15:18:45 +0100

    Added memory footprint computation

--- array_addition/main.cpp
+++ array_addition/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
 #include <math.h>
 #include <chrono>
 #include <vector>
 #include <numeric>
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+    vm_usage     = 0.0;
+    resident_set = 0.0;
+
+    // the two fields we want
+    unsigned long vsize;
+    long rss;
+    {
+        std::string ignore;
+        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
+        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> vsize >> rss;
+    }
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
+}
 
 int main(int argc, char const *argv[]) {
 
@@ -67,6 +90,10 @@ int main(int argc, char const *argv[]) {
     } else {
         std::cout << "2 arguments expected: exponent and repetition!" << std::endl;
     }
+
+    double vm, rss;
+    process_mem_usage(vm, rss);
+    std::cout << "VM: " << vm << " RSS: " << rss << std::endl;
     
     return 0;
 }

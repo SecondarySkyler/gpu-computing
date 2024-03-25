8aa173252a5c2d8495adc86bd083d97df89302f1
SecondarySkyler <skylerhunter6@gmail.com>
2024-03-25 09:52:04 +0100

    Removed useless code

--- array_addition/main.cpp
+++ array_addition/main.cpp
@@ -8,26 +8,6 @@
 #include <random>
 #include <type_traits>
 
-void process_mem_usage(double& vm_usage, double& resident_set)
-{
-    vm_usage     = 0.0;
-    resident_set = 0.0;
-
-    // the two fields we want
-    unsigned long vsize;
-    long rss;
-    {
-        std::string ignore;
-        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
-        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                >> ignore >> ignore >> vsize >> rss;
-    }
-
-    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-    vm_usage = vsize / 1024.0;
-    resident_set = rss * page_size_kb;
-}
 
 // Templating a random generator
 template<typename T, typename R = std::mt19937_64>
@@ -110,10 +90,6 @@ int main(int argc, char const *argv[]) {
     } else {
         std::cout << "2 arguments expected: exponent and repetition!" << std::endl;
     }
-
-    double vm, rss;
-    process_mem_usage(vm, rss);
-    std::cout << "VM: " << vm << " RSS: " << rss << std::endl;
     
     return 0;
 }

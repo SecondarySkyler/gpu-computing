c306e408f72c64ac50af0e7cc912471198b26d69
Cristian <skylerhunter6@gmail.com>
2024-04-13 15:45:41 +0200

    added subplots

--- matrix_transposition/stats.py
+++ matrix_transposition/stats.py
@@ -19,12 +19,16 @@ cluster_mean_time = cluster_data_frame.groupby(['Flag', 'Dimension']).mean().res
 for index, row in desktop_mean_time.iterrows():
     total_data = 8 * row['Dimension'] * row['Dimension']
     bandwidth = (total_data / row['Times (s)']) / 1e9
-    desktop_mean_time.at[index, 'Bandwidth (GB/s)'] =  bandwidth
+    desktop_mean_time.at[index, 'Bandwidth (GB/s)'] = bandwidth
+    if row['Flag'] == '-O3' and row['Dimension'] == 1024:
+        print(f"Desktop: {bandwidth} GB/s")
+
+        
 
 for index, row in cluster_mean_time.iterrows():
     total_data = 8 * row['Dimension'] * row['Dimension']
     bandwidth = (total_data / row['Times (s)']) / 1e9
-    cluster_mean_time.at[index, 'Bandwidth (GB/s)'] =  bandwidth
+    cluster_mean_time.at[index, 'Bandwidth (GB/s)'] = bandwidth
 
 
 # is the same for both dataframes
@@ -40,7 +44,8 @@ cluster_y_axis_O1 = cluster_mean_time[cluster_mean_time.Flag == '-O1']['Bandwidt
 cluster_y_axis_O2 = cluster_mean_time[cluster_mean_time.Flag == '-O2']['Bandwidth (GB/s)']
 cluster_y_axis_O3 = cluster_mean_time[cluster_mean_time.Flag == '-O3']['Bandwidth (GB/s)']
 
-fig, (desktop_plot, cluster_plot) = plt.subplots(2, 1, figsize=(6.4*1.5, 4.8*2))
+# fig, (desktop_plot, cluster_plot) = plt.subplots(2, 1, figsize=(6.4*1.5, 4.8*2))
+fig, (desktop_plot, cluster_plot) = plt.subplots(2, 1, figsize=(6.4, 4.8))
 fig.tight_layout(pad=3.0)
 
 desktop_plot.plot(x_axis.astype('str'), desktop_y_axis_O0.to_numpy(), label='-O0')
@@ -61,5 +66,5 @@ cluster_plot.legend()
 fig.supxlabel('Matrix Dimension')
 fig.supylabel('Bandwidth (GB/s)')
 
-plt.show()
-# plt.savefig('./report/img/naive_comparison_2.png', dpi=300)
+# plt.show()
+plt.savefig('./report/img/naive_comparison.png', dpi=300)

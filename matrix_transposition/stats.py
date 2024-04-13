ab221a23014eaee9e051c1369646e4b9307a835b
Cristian <skylerhunter6@gmail.com>
2024-04-13 10:41:49 +0200

    Removed M1 from comparison,  bandwidth calculation

--- matrix_transposition/stats.py
+++ matrix_transposition/stats.py
@@ -1,45 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-m1_data = 'csv/output_m1.csv'
 desktop_data = 'csv/desktop_naive.csv'
 cluster_data = 'csv/cluster_naive.csv'
 
 #load data
-m1_data_frame = pd.read_csv(m1_data, header=None)
 desktop_data_frame = pd.read_csv(desktop_data, header=None)
 cluster_data_frame = pd.read_csv(cluster_data, header=None)
 
-m1_data_frame.columns = ['Flag', 'Dimension', 'meanBandwidth', 'peakBandwidth']
-desktop_data_frame.columns = ['Flag', 'Dimension', 'meanBandwidth']
-cluster_data_frame.columns = ['Flag', 'Dimension', 'meanBandwidth']
+desktop_data_frame.columns = ['Flag', 'Dimension', 'Times (s)']
+cluster_data_frame.columns = ['Flag', 'Dimension', 'Times (s)']
+
+# Group by flag and dimension, then calculate the mean time
+desktop_mean_time = desktop_data_frame.groupby(['Flag', 'Dimension']).mean().reset_index()
+cluster_mean_time = cluster_data_frame.groupby(['Flag', 'Dimension']).mean().reset_index()
+
+# Calculate bandwidth
+for index, row in desktop_mean_time.iterrows():
+    total_data = 8 * row['Dimension'] * row['Dimension']
+    bandwidth = (total_data / row['Times (s)']) / 1e9
+    desktop_mean_time.at[index, 'Bandwidth (GB/s)'] =  bandwidth
+
+for index, row in cluster_mean_time.iterrows():
+    total_data = 8 * row['Dimension'] * row['Dimension']
+    bandwidth = (total_data / row['Times (s)']) / 1e9
+    cluster_mean_time.at[index, 'Bandwidth (GB/s)'] =  bandwidth
+
 
 # is the same for both dataframes
-x_axis = m1_data_frame.Dimension.unique()
+x_axis = desktop_mean_time.Dimension.unique()
 
-m1_y_axis_O0 = m1_data_frame[m1_data_frame.Flag == '-O0']['meanBandwidth']
-m1_y_axis_O1 = m1_data_frame[m1_data_frame.Flag == '-O1']['meanBandwidth']
-m1_y_axis_O2 = m1_data_frame[m1_data_frame.Flag == '-O2']['meanBandwidth']
-m1_y_axis_O3 = m1_data_frame[m1_data_frame.Flag == '-O3']['meanBandwidth']
+desktop_y_axis_O0 = desktop_mean_time[desktop_mean_time.Flag == '-O0']['Bandwidth (GB/s)']
+desktop_y_axis_O1 = desktop_mean_time[desktop_mean_time.Flag == '-O1']['Bandwidth (GB/s)']
+desktop_y_axis_O2 = desktop_mean_time[desktop_mean_time.Flag == '-O2']['Bandwidth (GB/s)']
+desktop_y_axis_O3 = desktop_mean_time[desktop_mean_time.Flag == '-O3']['Bandwidth (GB/s)']
 
-desktop_y_axis_O0 = desktop_data_frame[desktop_data_frame.Flag == '-O0']['meanBandwidth']
-desktop_y_axis_O1 = desktop_data_frame[desktop_data_frame.Flag == '-O1']['meanBandwidth']
-desktop_y_axis_O2 = desktop_data_frame[desktop_data_frame.Flag == '-O2']['meanBandwidth']
-desktop_y_axis_O3 = desktop_data_frame[desktop_data_frame.Flag == '-O3']['meanBandwidth']
+cluster_y_axis_O0 = cluster_mean_time[cluster_mean_time.Flag == '-O0']['Bandwidth (GB/s)']
+cluster_y_axis_O1 = cluster_mean_time[cluster_mean_time.Flag == '-O1']['Bandwidth (GB/s)']
+cluster_y_axis_O2 = cluster_mean_time[cluster_mean_time.Flag == '-O2']['Bandwidth (GB/s)']
+cluster_y_axis_O3 = cluster_mean_time[cluster_mean_time.Flag == '-O3']['Bandwidth (GB/s)']
 
-cluster_y_axis_O0 = cluster_data_frame[cluster_data_frame.Flag == '-O0']['meanBandwidth']
-cluster_y_axis_O1 = cluster_data_frame[cluster_data_frame.Flag == '-O1']['meanBandwidth']
-cluster_y_axis_O2 = cluster_data_frame[cluster_data_frame.Flag == '-O2']['meanBandwidth']
-cluster_y_axis_O3 = cluster_data_frame[cluster_data_frame.Flag == '-O3']['meanBandwidth']
-
-fig, (m1_plot, desktop_plot, cluster_plot) = plt.subplots(3, 1, figsize=(6.4*1.5, 4.8*2))
+fig, (desktop_plot, cluster_plot) = plt.subplots(2, 1, figsize=(6.4*1.5, 4.8*2))
 fig.tight_layout(pad=3.0)
-
-m1_plot.plot(x_axis.astype('str'), m1_y_axis_O0.to_numpy(), label='-O0')
-m1_plot.plot(x_axis.astype('str'), m1_y_axis_O1.to_numpy(), label='-O1')
-m1_plot.plot(x_axis.astype('str'), m1_y_axis_O2.to_numpy(), label='-O2')
-m1_plot.plot(x_axis.astype('str'), m1_y_axis_O3.to_numpy(), label='-O3')
-m1_plot.set_title('Apple M1')
 
 desktop_plot.plot(x_axis.astype('str'), desktop_y_axis_O0.to_numpy(), label='-O0')
 desktop_plot.plot(x_axis.astype('str'), desktop_y_axis_O1.to_numpy(), label='-O1')
@@ -53,13 +55,11 @@ cluster_plot.plot(x_axis.astype('str'), cluster_y_axis_O2.to_numpy(), label='-O2
 cluster_plot.plot(x_axis.astype('str'), cluster_y_axis_O3.to_numpy(), label='-O3')
 cluster_plot.set_title('Intel Xeon Silver 4309y')
 
-m1_plot.legend()
 desktop_plot.legend()
 cluster_plot.legend()
-flags = ['-O0', '-O1', '-O2', '-O3']
 
 fig.supxlabel('Matrix Dimension')
 fig.supylabel('Bandwidth (GB/s)')
 
-# plt.show()
-plt.savefig('./report/img/naive_comparison_2.png', dpi=300)
+plt.show()
+# plt.savefig('./report/img/naive_comparison_2.png', dpi=300)
